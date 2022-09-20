c39606d77faf1131a54f3c4cce1758e53c548293
redi961 <gkdlvj23@naver.com>
2022-09-20 14:13:54 +0900

    20220920

---
+++ edu/src/edu/Test02.java
@@ -0,0 +1,15 @@
+package edu;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		int depositAmount;
+		char a;
+		a = 'e';
+		
+		depositAmount = 50000;
+		System.out.println(a);
+		
+	}
+
+}
---
+++ edu/src/edu/Test21.java
@@ -0,0 +1,24 @@
+package edu;
+
+public class Test21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 2;
+		char op = '/';
+		
+		if (op =='+') {
+			System.out.println(a + b);
+		}
+		else if (op =='-') {
+			System.out.println(a - b);
+		}
+		else if (op == '*') {
+			System.out.println(a * b);
+		}
+		else if (op == '/') {
+			System.out.println(a / b);
+		}
+	}
+}
