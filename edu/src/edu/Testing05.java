a347f88c752c4f9ea2055fe2f48593833ab99a92
redi961 <gkdlvj23@naver.com>
2022-09-22 14:09:25 +0900

    20220922_java_studying

--- edu/src/edu/Testing05.java
+++ edu/src/edu/Testing05.java
@@ -21,6 +21,7 @@ public class Testing05 {
 			}
 		}
 		for(int i = 0; i<a.length; i++) {
+			
 			for (int j=0;j<b[0].length;j++) {
 				for (int k=0; k <a[0].length; k++ ) {
 					c [i][j] += a[i][k] * b[k][j];
---
+++ edu/src/edu/gugudan.java
@@ -0,0 +1,10 @@
+package edu;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
