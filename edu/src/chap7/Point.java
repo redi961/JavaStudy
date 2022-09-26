335e5404bc3e2e995a08002e66ee9f1b04b3628f
redi961 <gkdlvj23@naver.com>
2022-09-26 10:40:09 +0900

    class 상속 연습 -1

---
+++ edu/src/chap7/Point.java
@@ -0,0 +1,16 @@
+package chap7;
+
+public class Point {
+	private int x,y;
+	
+	public void setPoint (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void getPoint () {
+		System.out.println(" x : " + x + "\t y : " + y);
+	}
+}
+
+
---
+++ edu/src/chap7/Student.java
@@ -0,0 +1,40 @@
+package chap7;
+
+public class Student {
+	
+	private String name;
+	private int age;
+	private String dept;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge (int age) {
+		this.age = age;
+	}
+	
+	public String getDept() {
+		return dept;
+	}
+	
+	public void setDept (String dept) {
+		this.dept = dept;
+	}
+	
+	public static void main(String[] args) {
+		/*Emplyoee Class에서 만들어진 Person Class 호출 가능*/
+		Person p = new Person();
+		p.setAge(25);
+		System.out.println("Age : " + p.getAge());
+	}
+
+}
