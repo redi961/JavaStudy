4cd8118d160b49d28317bb383d9562e4d417ea1b
redi961 <gkdlvj23@naver.com>
2022-10-17 14:12:13 +0900

    iterator 활용문 보충

--- edu/src/Merge/MergeList.java
+++ edu/src/Merge/MergeList.java
@@ -73,32 +73,30 @@ public class MergeList {
 				list1.add("뉴욕");
 
 				ArrayList<String> list2 = new ArrayList<String>();
-				list2.add("런던");
+				list2.add("뉴욕");
 				list2.add("런던");
 				list2.add("로마");
 				list2.add("방콕");
 				list2.add("북경");
 				list2.add("도쿄");
-				list2.add("서울");
+				list2.add("런던");
 				list2.add(1, "LA");
 				
-				
-				System.out.println("collection.sort()::");
 			    Collections.sort(list1);
-			    System.out.println("list1::");
-			    for ( String city: list1)
+			    System.out.print("list1 :: ");
+			    for (String city: list1)
 			    	System.out.print(city+ " ");
-			    System.out.println();
-			    System.out.println("list2::");
+			    System.out.println("");
+			    System.out.print("list2 :: ");
 			    Collections.sort(list2);
-			    for ( String city: list2)
+			    for (String city: list2)
 			    	System.out.print(city+ " ");
 				ArrayList<String> list3 = new ArrayList<String>();
 				
-				String []lst1 = new String[0];
-				// 리스트르 Array로 변환하며 파라메터로 들어가는 인자와 Size를 맞춤
+				// 리스트를 Array로 변환하며 파라메터로 들어가는 인자와 Size를 맞춤
 				// 파라메터로 들어가는 인자보다 list의 Size가 클시 list의 사이즈를 유지하며
 				// list의 Size가 인자보다 작을시 인자의 Size로 Array를 구축한다.
+				String []lst1 = new String[0];
 				lst1 = list1.toArray(lst1);
 				String []lst2 = new String[0];
 				lst2 = list2.toArray(lst2);
@@ -106,37 +104,75 @@ public class MergeList {
 				
 				int count = lst1.length;
 				
-				Iterator<String> iter1 = list1.iterator();
-				while(iter1.hasNext()) {
-					String obj = (String)iter1.next();
-					list3.add(obj);
-				}
-				
-				Iterator<String> iter2 = list2.iterator();
-				while(iter2.hasNext()) {
-					String obj = (String)iter2.next();
-					list3.add(obj);
-				}
 				
 				Collections.sort(list1);
 				Collections.sort(list2);
-				
-				Iterator<String> iter3 = list3.iterator();
-				
-//				while (iter3.hasNext()) {
-//					count = 0;
-//					String str = iter3.next();
-//					for (i=0; i < list3.size(); i++) {
-//						if (list3.get(i) == str) {
-//							count++;
-//						}
-//					}
-//					if (count >= 2) {
-//						iter3.remove();
-//					}
-//					count =0;
+				Iterator<String> iter1 = list1.iterator();
+//				while(iter1.hasNext()) {
+//					String obj = (String)iter1.next();
+//					list3.add(obj);
 //				}
 				
+				Iterator<String> iter2 = list2.iterator();
+//				while(iter2.hasNext()) {
+//					String obj = (String)iter2.next();
+//					list3.add(obj);
+//				}
+
+				System.out.println("");
+
+				String st1 = iter1.next();
+				String st2 = iter2.next();
+				while (iter1.hasNext() && iter2.hasNext()) {
+					if(st1.compareTo(st2) < 0) 
+					{
+						list3.add(st1);
+						st1 = iter1.next();
+					}
+					else if (st1.compareTo(st2) > 0) 
+					{
+						list3.add(st2);
+						st2 = iter2.next();
+					}else 	{
+						list3.add(st1);
+						st1 = iter1.next();
+						st2 = iter2.next();
+					}
+				while(iter1.hasNext()) {
+						list3.add(st1);
+						st1 = iter1.next();
+					}
+				while(iter2.hasNext()) {
+						list3.add(st2);
+						st2 = iter2.next();
+					}
+				}
+				
+				Iterator<String> iter3 = list3.iterator();
+				System.out.print("MergeList :: ");
+				for (String city : list3)
+					System.out.print(city + " ");
+				
+				while (iter3.hasNext()) {
+					count = 0;
+					String str = iter3.next();
+					for (i=0; i < list3.size(); i++) {
+						//Str문과 list3의 i번째 문과의 비교
+						//비교를 시작할때 자신과 비교함으로써 count 값이 1이되고 추가 중복 발생시 count2 값이 2가되며, 해당 배열을 삭제처리함
+						// 배열 검사 확인용 코드
+						// System.out.println(str + "//" + list3.get(i) + " C : " + count );
+						if (list3.get(i) == str) {
+							count++;
+						}
+					}
+					if (count >= 2) {
+						iter3.remove();
+					}
+					count =0;
+				}
+				
+				Collections.sort(list3);
+//				
 				lst1 = removeDuplicate(lst1);
 				lst2 = removeDuplicate(lst2);
 				
@@ -163,7 +199,7 @@ public class MergeList {
 					System.out.print(city + " ");
 				
 				System.out.println("");
-				System.out.print("MergeList :: ");
+				System.out.print("Sort MergeList :: ");
 				for (String city : list3)
 					System.out.print(city + " "); 
 	}
